b0e88c879c67111df11339b35b3bbd35bb8552d7
Allyssa Dalton <allyssamarie06@icloud.com>
2025-10-21 11:52:53 -0400

    fixed TextureLoader

--- public/main.js
+++ public/main.js
@@ -426,7 +426,7 @@ window.addEventListener('resize', () => {
 });
 
 // 🌲 Load tree bark texture
-const treeBarkTexture = textureLoader.load('assets/tree.png');
+const treeBarkTexture = TextureLoader.load('assets/tree.png');
 treeBarkTexture.wrapS = THREE.RepeatWrapping;
 treeBarkTexture.wrapT = THREE.RepeatWrapping;
 treeBarkTexture.repeat.set(1, 2); // tile vertically a bit
